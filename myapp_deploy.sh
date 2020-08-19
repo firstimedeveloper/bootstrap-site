22e762c41366b972fe89ac691362338f2900ad7d
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-08-19 21:46:00 +0900

    Using wsl to connect to server.

--- myapp_deploy.sh
+++ myapp_deploy.sh
@@ -4,9 +4,10 @@ echo "==== Starting deploy process to server ===="
 $cd $1
 
 echo "==== Deploying files to server ===="
-rsync -zarv --include="*/" ./ root@junhyukhan.com:/var/www/junhyukhan/
+rsync -zarv --include="*/" ./ jun@junhyukhan.com:/var/www/junhyukhan/
 
 echo " Restarting Caddy server..."
-ssh root@157.230.242.2 "sudo service caddy restart"
+ssh jun@157.230.242.2 "sudo -S service caddy restart"
+
 echo "==== Done deploying lenslocked.com ===="
 
